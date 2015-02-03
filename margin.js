fada5c1d6aaf6d0e2a23351f01b4b484eb5a30f3
Chau Hoang <kidphys@gmail.com>
2015-02-03 18:15:57 +0700

    Support submit json data to server via get

--- margin.js
+++ margin.js
@@ -1,6 +1,6 @@
 var marginApp = angular.module('MarginApp', []);
 
-marginApp.controller('MarginAppController', function ($scope) {
+marginApp.controller('MarginAppController', ['$scope', '$http', function ($scope, $http) {
     $scope.account = {};
     $scope.account.stockBook = [
         {'symbol': 'VND', 'amount': 1000000},
@@ -25,10 +25,17 @@ marginApp.controller('MarginAppController', function ($scope) {
     ];
 
 
-    $scope.new_stock = "VND 100000";
+    $scope.new_stock = "";
     $scope.add_stock = function(stockBook, str){
         var stock = str.split(' ');
-        stockBook.push({'symbol': stock[0], 'amount': parseInt(stock[1])});
+        var amount = parseInt(stock[1]);
+        for (var idx in stockBook){
+            if (stockBook[idx].symbol == stock[0]){
+                stockBook[idx].amount = amount;
+                return;
+            }
+        }
+        stockBook.push({'symbol': stock[0], 'amount': amount});
     };
 
     $scope.removeStock = function(stockBook, stock){
@@ -38,10 +45,17 @@ marginApp.controller('MarginAppController', function ($scope) {
         }
     };
 
-    $scope.new_rate_str = "SHS 0.5";
+    $scope.new_rate_str = "";
     $scope.add_rate = function(rates, str){
         var rate = str.split(' ');
-        rates.push({'symbol': rate[0], 'rate': parseFloat(rate[1])});
+        var rate_number = parseFloat(rate[1]);
+        for (var idx in rates){
+            if (rates[idx].symbol == rate[0]){
+                rates[idx].rate = rate_number;
+                return;
+            }
+        }
+        rates.push({'symbol': rate[0], 'rate': rate_number});
     }
 
     $scope.remove_rate = function(rates, rate){
@@ -50,4 +64,14 @@ marginApp.controller('MarginAppController', function ($scope) {
             rates.splice(index, 1);
         }
     }
-});
+
+    $scope.send_data = function(){
+        console.log("Calculating purchasing power");
+        $.ajax({
+            type: 'get',
+            url: 'http://localhost:5000/pp',
+            data: {'stockBook': JSON.stringify($scope.account.stockBook)},
+            dataType: 'json',
+        });
+    }
+}]);
